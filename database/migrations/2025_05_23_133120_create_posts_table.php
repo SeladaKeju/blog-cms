5bf88c71b9864e4c5fa3ce00beb6a8ca9023e3c0
SeladaKeju <rizqiadit2430@gmail.com>
2025-05-23 20:43:18 +0700

    init: setup Laravel + React with Breeze

---
+++ database/migrations/2025_05_23_133120_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('excerpt')->nullable();
+            $table->longtext('content');
+            $table->string('thumbnail');
+            $table->string('generate_image');
+            $table->boolean('is_published');
+            $table->datetime('published_at')->nullable();
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
